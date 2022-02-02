20c5a33d1f304b586a740c4fc7532ef5146cc9eb
M10beretta <Mberetta.gm@gmail.com>
2022-02-02 12:46:23 +0300

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mockito
+
+
+### Resources
+
+- [Class Mockito [javadoc.io]](https://javadoc.io/doc/org.mockito/mockito-core/latest/org/mockito/Mockito.html)
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+//Let's import Mockito statically so that the code looks clearer
+import static org.mockito.Mockito.*;
+
+public class MainTest {
+
+    /**
+     * 1. Let's verify some behaviour!
+     */
+    @Test
+    void test1() {
+        //mock creation
+        List mockedList = mock(List.class);
+
+        //using mock object
+        mockedList.add("one");
+        mockedList.clear();
+
+        //verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+    }
+
+
+    /**
+     * 2. How about some stubbing?
+     */
+    @Test
+    void test2() {
+        //You can mock concrete classes, not just interfaces
+        LinkedList mockedList = mock(LinkedList.class);
+
+        //stubbing
+        when(mockedList.get(0)).thenReturn("first");
+        when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+        //following prints "first"
+        System.out.println(mockedList.get(0));
+
+        //following throws runtime exception
+        System.out.println(mockedList.get(1));
+
+        //following prints "null" because get(999) was not stubbed
+        System.out.println(mockedList.get(999));
+
+        //Although it is possible to verify a stubbed invocation, usually it's just redundant
+        //If your code cares what get(0) returns, then something else breaks (often even before verify() gets executed).
+        //If your code doesn't care what get(0) returns, then it should not be stubbed.
+        verify(mockedList).get(0);
+    }
+}
